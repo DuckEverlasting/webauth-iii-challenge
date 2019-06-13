ec219ed08451305089daa0eb9d47aec51e593551
Matt Klein <mklein246@gmail.com>
2019-06-13 14:14:28 -0700

    small changes

--- client/src/auth/requiresAuth.js
+++ client/src/auth/requiresAuth.js
@@ -3,13 +3,12 @@ import axios from 'axios';
 
 const token = localStorage.getItem('jwt');
 
-// could be process.env.API_URL
 axios.defaults.baseURL = 'http://localhost:4000/api';
 
 export default function(Component) {
   return class Authenticated extends React.Component {
     render() {
-      const notLoggedIn = <div>Please login to see our users</div>;
+      const notLoggedIn = <div>You should log in first</div>;
 
       return <>{token ? <Component {...this.props} /> : notLoggedIn}</>;
     }
